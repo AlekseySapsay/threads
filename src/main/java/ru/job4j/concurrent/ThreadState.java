de974d37576a2321478cad845ac9625975d7bbc8
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-22 23:37:09 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+/**
+ * https://job4j.ru/profile/exercise/71/task-view/399
+ * <p>
+ * Изучение жизненного цикла нитей
+ * Thread state
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 22.11.2021
+ */
+public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
+        first.start();
+        second.start();
+        second.join();
+        first.join();
+
+        System.out.println("Work done!");
+    }
+}
