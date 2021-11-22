95f0b4702217643bc745f143678a0f63037da7f0
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-22 21:48:49 +0300

    first commit

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+/**
+ * https://job4j.ru/profile/exercise/71/task-view/398
+ * <p>
+ * Изучение Thread.start()
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 22.11.2021
+ */
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(() -> System.out.println(
+                Thread.currentThread().getName()));
+        another.start();
+        Thread second = new Thread(() -> System.out.println(
+                Thread.currentThread().getName()));
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
