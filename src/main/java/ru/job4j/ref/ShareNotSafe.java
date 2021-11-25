0925188d32bbc1452dfcfc5f3cb33a11f7df32be
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-25 13:48:06 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/ShareNotSafe.java
@@ -0,0 +1,70 @@
+package ru.job4j.ref;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://job4j.ru/profile/exercise/72/task-view/408
+ * Thread без общих ресурсов
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 25.11.2021
+ */
+
+public class ShareNotSafe {
+    public static void main(String[] args) throws InterruptedException {
+        UserCache cache = new UserCache();
+        User user = User.of("name");
+        cache.add(user);
+        System.out.println(cache.findById(1).getName());
+        Thread first = new Thread(() -> {
+            user.setName("rename");
+        }
+        );
+        System.out.println("User from ShareNotSafe before first.start: "
+                + user.getName());
+        first.start();
+        first.join();
+        System.out.println("User from ShareNotSafe after first.start: "
+                + user.getName());
+        System.out.println(cache.findById(1).getName());
+
+        user.setName("Ivan in ShareNotSafe");
+        System.out.println(user.getName());
+
+        cache.findById(1).setName("rename 100500");
+        System.out.println("user name from cache : "
+                + cache.findById(1).getName());
+
+        cache.findById(1).setName("rename 100500");
+
+        User user1 = new User();
+        user1.setName("rename 100500");
+        cache.add(user1);
+        System.out.println("user name from cache : "
+                + cache.findById(2).getName());
+
+        System.out.println("cache.findAll() " + cache.findAll().toString());
+        //System.out.println("cache.findAll() " + cache.findAll().toString());
+        //System.out.println(cache.findById(0).getName());
+        //System.out.println("cache.findAll() " + cache.findAll().toString());
+        System.out.println("cache.findById(1).getName(): " + cache.findById(1).getName());
+        System.out.println("cache.findById(2).getName(): " + cache.findById(2).getName());
+
+        cache.findById(1).setName("New Name");
+        System.out.println("cache.findById(1).getName() after rename: " + cache.findById(1).getName());
+
+        //System.out.println("cache.findById(2).getName(): " + cache.findById(2).getName());
+        List<User> userList = new ArrayList<User>();
+        userList.addAll(cache.findAll());
+        //userList.remove(user);
+        System.out.println("userList.toString() " + userList.toString());
+
+
+
+    }
+}
---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,41 @@
+package ru.job4j.ref;
+
+import net.jcip.annotations.ThreadSafe;
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * https://job4j.ru/profile/exercise/72/task-view/408
+ * Thread без общих ресурсов
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 25.11.2021
+ */
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> arrayList = new ArrayList();
+        for (User user : users.values()) {
+            arrayList.add(User.of(user.getName()));
+        }
+        return arrayList;
+    }
+}
