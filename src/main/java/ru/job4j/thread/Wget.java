bcfe13b8aa11453f09359b2f41e544f201ad9fc5
Сапсай Алексей Юрьевич <ASapsay@alfabank.ru>
2022-09-18 23:11:39 +0400

    4. Скачивание файла с ограничением. [#144271 #247742] провел рефакторинг, теперь расчет скорости удобнее

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -5,39 +5,68 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+import static java.lang.System.currentTimeMillis;
+
 public class Wget implements Runnable {
     private final String url;
+
     private final int speed;
 
-    public Wget(String url, int speed) {
+    private final String fileName;
+
+    private final String fileExtension;
+
+
+    public Wget(String url, int speed, String fileName, String fileExtension) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
+        this.fileExtension = fileExtension;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("src/main/java/ru/job4j/thread/file.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream("src/main/java/ru/job4j/thread/"
+                     + fileName + "." + fileExtension)) {
+
+            int downloadData = 0;
             byte[] dataBuffer = new byte[speed];
             int bytesReadied;
+            int totalSizeBytes = 0;
+            long startTimeMs = 0;
+            int totalTimeSeconds = 0;
+            float deltaTimeSec = 0;
+
+            System.out.println("Start loading ... ");
+            startTimeMs = System.currentTimeMillis();
 
             while ((bytesReadied = in.read(dataBuffer, 0, speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesReadied);
-                if (bytesReadied < speed) {
-                    Thread.sleep(bytesReadied * 1_000L / speed);
+                downloadData = bytesReadied;
+                deltaTimeSec = (float) ((currentTimeMillis() - startTimeMs) / 1_000_000.0);
+                if ((downloadData >= speed) && (deltaTimeSec < 1)) {
+                    long threadPauseMs = (long) ((1 - deltaTimeSec) * 1000);
+                    Thread.sleep(threadPauseMs);
                 }
-                Thread.sleep(1000);
+                totalSizeBytes += downloadData;
+                totalTimeSeconds += 1;
             }
+            System.out.println("total size bytes : " + totalSizeBytes);
+            System.out.println("total time seconds : " + totalTimeSeconds);
+            System.out.println("average speed : " + totalSizeBytes / (totalTimeSeconds - 1));
+            System.out.println("Loading completed ...");
+
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     private static void validationArguments(String[] args) {
-        if (args.length < 2) {
+        if (args.length != 4) {
             throw new IllegalArgumentException("Not enough arguments, add additional arguments and try again");
         }
-        if (!args[1].matches("^(100)$")) {
+        if (!args[1].matches("^\\d+$")) {
             throw new IllegalArgumentException("Not enable data format");
         }
     }
@@ -46,7 +75,10 @@ public class Wget implements Runnable {
         validationArguments(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args[2];
+        String fileExtension = args[3];
+
+        Thread wget = new Thread(new Wget(url, speed, fileName, fileExtension));
         wget.start();
         wget.join();
     }
